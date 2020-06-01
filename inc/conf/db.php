fc5c3c871407cc37c68d2c7aebf6dd4bb66b5b5c
Sudeep Gurung <webtree22@gmail.com>
2020-06-01 13:48:46 +0545

    wip

--- inc/conf/db.php
+++ inc/conf/db.php
@@ -7,6 +7,6 @@ $conn = new mysqli($server,$user,$pw,$db);
 $conn->set_charset('utf8mb4');
 // $BACKENDSERVER  = 'http://127.0.0.1/';
 // $FRONTEND = "http://localhost/";
-$BACKENDSERVER  = 'http://auto.steemdb.online/';
-$FRONTEND = "http://auto.steemdb.online/";
+$BACKENDSERVER  = 'https://auto.steemdb.online/';
+$FRONTEND = "https://auto.steemdb.online/";
 ?>
